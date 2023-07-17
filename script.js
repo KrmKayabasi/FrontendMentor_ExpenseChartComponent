8bcdbdcb4b31f2053bd9baf6932751b1827e2334
KrmKayabasi <111309350+KrmKayabasi@users.noreply.github.com>
2023-07-17 18:42:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let max = 0;
+let maxday = "";
+fetch('./data.json')
+    .then(data => data.json())
+    .then(res => {
+        for(let gun of res){
+            let dayname = gun.day;
+            let bar = document.getElementById(dayname).firstElementChild;
+            let h =gun.amount *2;
+            let s = h.toString();
+            bar.style.height = s + "px";
+            bar.setAttribute("money", "$" + gun.amount);
+
+            if(h > max){
+                maxday = dayname;
+                max = h;
+                
+            }
+            
+            
+        }
+
+document.getElementById(maxday).firstElementChild.style.background = "hsl(186, 34%, 60%)";
+});
+console.log(maxday)
